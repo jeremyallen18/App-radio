39984723461fdc5a3115ad5af8f649104d5f91b2
AbrahamJoveRode <abri.jovellano@gmail.com>
2026-08-10 12:35:52 -0600

    Ajustes

--- lib/utils/api_config.dart
+++ lib/utils/api_config.dart
@@ -1,6 +1,1 @@
-// Cambia solo esta constante para apuntar la app a otro servidor/IP/dominio.
-<<<<<<< Updated upstream
-const String kBaseUrl = 'http://192.168.3.74/hive-backend';
-=======
 const String kBaseUrl = 'http://192.168.100.251/hive-backend';
->>>>>>> Stashed changes
